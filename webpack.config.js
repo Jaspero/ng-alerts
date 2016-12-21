a4c92bed6f144664df5e6aa9bd0a655e02a44ea6
Filip Lauc <filip.lauc93@gmail.com>
2016-12-21 11:57:07 +0100

    feat: Initial setup

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+/**
+ * Adapted from angular2-webpack-starter
+ * and ng2-translate
+ */
+
+const helpers = require('./config/helpers'),
+    webpack = require('webpack');
+
+/**
+ * Webpack Plugins
+ */
+const ProvidePlugin = require('webpack/lib/ProvidePlugin');
+const DefinePlugin = require('webpack/lib/DefinePlugin');
+const LoaderOptionsPlugin = require('webpack/lib/LoaderOptionsPlugin');
+
+module.exports = {
+    devtool: 'inline-source-map',
+
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+
+    entry: helpers.root('ng2-form-builder.ts'),
+
+    output: {
+        path: helpers.root('bundles'),
+        publicPath: '/',
+        filename: 'ng2-form-builder.umd.js',
+        libraryTarget: 'umd',
+        library: 'ng2-form-builder'
+    },
+
+    // require those dependencies but don't bundle them
+    externals: [/^\@angular\//, /^rxjs\//],
+
+    module: {
+        rules: [{
+            enforce: 'pre',
+            test: /\.ts$/,
+            loader: 'tslint-loader',
+            exclude: [helpers.root('node_modules')]
+        }, {
+            test: /\.ts$/,
+            loader: 'awesome-typescript-loader?declaration=false',
+            exclude: [/\.e2e\.ts$/]
+        }]
+    },
+
+    plugins: [
+        // fix the warning in ./~/@angular/core/src/linker/system_js_ng_module_factory_loader.js
+        new webpack.ContextReplacementPlugin(
+            /angular(\\|\/)core(\\|\/)(esm(\\|\/)src|src)(\\|\/)linker/,
+            helpers.root('./src')
+        ),
+
+        new webpack.LoaderOptionsPlugin({
+            options: {
+                tslintLoader: {
+                    emitErrors: false,
+                    failOnHint: false
+                }
+            }
+        })
+    ]
+};
