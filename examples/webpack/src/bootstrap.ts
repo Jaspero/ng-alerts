97fcf2750623826c1702e951e53dda26a54367b0
Filip Lauc <filip.lauc93@gmail.com>
2017-01-19 22:46:16 +0100

    chore(example): Removed :heavy_minus_sign:

--- examples/webpack/src/bootstrap.ts
+++
@@ -1,8 +0,0 @@
-import 'core-js';
-import 'zone.js/dist/zone';
-import 'ts-helpers';
-
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-import {AppModule} from './app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule);
