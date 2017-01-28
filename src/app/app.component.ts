0b05145bf7586cc47ac16413ddbf9515ca5c7f9c
Filip Lauc <filip.lauc93@gmail.com>
2017-01-28 11:31:49 +0100

    feat(config): Added styles :construction:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
     selector: 'app',
+    styleUrls: ['./../assets/scss/main.scss'],
+    encapsulation: ViewEncapsulation.None,
     templateUrl: './app.html'
 })
 export class AppComponent {}
